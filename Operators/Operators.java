abb96b3aee8062727b5468597fd7a2ff3984a177
yakupacs <ykpacs@gmail.com>
2023-08-15 15:04:46 +0300

    Updated.

---
+++ Operators/Operators.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Operators {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		System.out.println(x & y);
+		System.out.println(x | y);
+	}
+}
