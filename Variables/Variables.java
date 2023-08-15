4d91377eaaaf528c3f2625ff92be1c66697c0100
yakupacs <ykpacs@gmail.com>
2023-08-15 14:14:19 +0300

    Updated.

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -1,20 +1,21 @@
 package Variables;
-public class Main {
+public class Variables {
     public static void main(String[] args) {
 		String myString = "My string!";
-		char myChar = 'I';
-		int myInt = 42;
-		final int myFinalInt = 422; // read-only = final
-		double myDouble = 42.42;
-		float myFloat = 42.42f;
+		char myChar = 'A';
+		int myInt = 100;
+		final int myFinalInt = 100; // read-only = final
+		double myDouble = 10.10;
+		float myFloat = 10.10f;
 		boolean myBool = true;
 
-		System.out.println("Hello " + myInt);
+		System.out.println(myString + myInt);
 		System.out.println("Hello " + myString);
 		System.out.println(myFinalInt);
 		System.out.println(myFloat);
 		System.out.println(myBool);
 		System.out.println(myChar);
 		System.out.println(myDouble);
+		System.out.println(myDouble + myChar);
     }
 }
