dd9567812fb76bf80c39ad429f8b4cb4b5d90564
yakupacs <ykpacs@gmail.com>
2023-08-15 16:46:06 +0300

    Math added.

---
+++ Math/MathMain.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class MathMain {
+	public static void main(String[] args) {
+		System.out.println(Math.max(5, 10)); // 10
+		System.out.println(Math.min(5, 10)); // 5
+		System.out.println(Math.abs(-42)); // 42
+		System.out.println(Math.sqrt(4)); // 2.0
+		System.out.println(Math.pow(2, 6)); // 64.0
+		System.out.println(Math.floor(3.9)); // 3.0
+		System.out.println(Math.random() * 100); // Random float number(0-1)
+		System.out.println(Math.round(3.6)); // 4
+	}
+}
