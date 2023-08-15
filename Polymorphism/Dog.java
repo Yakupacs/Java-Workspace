48132b987a36f628b851c5d0d37b049b96a35c78
yakupacs <ykpacs@gmail.com>
2023-08-15 18:57:26 +0300

    Polymorphism added.

---
+++ Polymorphism/Dog.java
@@ -0,0 +1,13 @@
+package Polymorphism;
+
+public class Dog extends Animal{
+	public Dog(String name)
+	{
+		super(name);
+	}
+	@Override
+	public void makeSound()
+	{
+		System.out.println("Hav Hav!");
+	}
+}
--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -4,8 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Animal animal = new Animal("animal");
 		Cat kedi = new Cat("cat");
+		Dog dog = new Dog("dog");
 
 		animal.makeSound();
 		kedi.makeSound();
+		dog.makeSound();
 	}
 }
