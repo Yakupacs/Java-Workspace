f60cf2bedf346903664f15b9dd2ab41cc4ac3132
yakupacs <ykpacs@gmail.com>
2023-08-16 12:20:42 +0300

    Date updated.

--- Date/Date.java
+++ Date/Date.java
@@ -8,14 +8,19 @@ import java.time.format.DateTimeFormatter;
 public class Date {
 	public static void main(String[] args) {
 		LocalDate myObj = LocalDate.now();
-		System.out.println("Now date is " + myObj);
+		System.out.println("Now date is " + myObj); // Now date is 2023-08-16
 
 		myObj = LocalDate.of(2000, Month.APRIL, 9);
-		System.out.println("My birthday date is " + myObj);
+		System.out.println("My birthday date is " + myObj); // My birthday date is 2000-04-09
 
 		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 		LocalDateTime time = LocalDateTime.now();
 		String formattedDate = time.format(myFormatObj);
-		System.out.println("Now time is " + formattedDate);
+		System.out.println("Now time is " + formattedDate); // Now time is 16-08-2023 12:19:39
+
+		myFormatObj = DateTimeFormatter.ofPattern("MM-yyyy-dd ss:mm:HH");
+		time = LocalDateTime.now();
+		formattedDate = time.format(myFormatObj);
+		System.out.println("Now time is " + formattedDate); // Now time is 08-2023-16 39:19:12
 	}
 }
